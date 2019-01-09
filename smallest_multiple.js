6be981a755b521590dca003fc343b28b925db87c
Gene83 <gene83@protonmail.com>
2019-01-09 09:13:46 -1000

    use for loop nested in while loop to pass tests

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,26 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
-  // do work here
+module.exports = function(ceiling) {
+  const numbers = [];
+  let result = 1;
+  let solved = false;
 
+  for (let i = ceiling; i > 0; i--) {
+    numbers.push(i);
+  }
 
-  return 0;
+  while (!solved) {
+    for (let i = 0; i < numbers.length; i++) {
+      if (result % numbers[i] === 0) {
+        solved = true;
+      } else {
+        solved = false;
+        result++;
+        break;
+      }
+    }
+  }
+
+  return result;
 };
